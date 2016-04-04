3a8662f695255cbd29afb6c173d3d3f59d499fea
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-04-04 15:09:21 +0200

    WIP work on work query

--- redef/services/src/test/java/no/deichman/services/search/WorkModelToIndexMapperTest.java
+++ redef/services/src/test/java/no/deichman/services/search/WorkModelToIndexMapperTest.java
@@ -16,11 +16,11 @@ import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONAs;
 public class WorkModelToIndexMapperTest {
     private String comparisonJsonDocument = "{\n" +
             "  \"work\": {\n" +
-            "    \"uri\": \"http:\\/\\/192.168.50.12:8005\\/work\\/w4e5db3a95caa282e5968f68866774e20\",\n" +
+            "    \"uri\": \"%1$s\",\n" +
             "    \"contributor\": {\n" +
             "      \"uri\": \"_:b0\",\n" +
             "      \"author\": {\n" +
-            "        \"uri\": \"http:\\/\\/192.168.50.12:8005\\/person\\/h10834700\",\n" +
+            "        \"uri\": \"%2$s\",\n" +
             "        \"birthYear\": \"1957\",\n" +
             "        \"name\": \"Ragde, Anne B.\"\n" +
             "      }\n" +
@@ -54,8 +54,7 @@ public class WorkModelToIndexMapperTest {
         XURI publicationXuri1 = new XURI("http://192.168.50.12:8005/publication/p594502562255");
         XURI publicationXuri2 = new XURI("http://192.168.50.12:8005/publication/p735933031021");
 
-        String inputGraph = "" +
-                "@prefix ns1: <http://data.deichman.no/duo#> .\n" +
+        String inputGraph = "@prefix ns1: <http://data.deichman.no/duo#> .\n" +
                 "@prefix ns2: <http://deichman.no/ontology#> .\n" +
                 "@prefix ns4: <http://192.168.50.12:8005/raw#> .\n" +
                 "@prefix ns5: <http://data.deichman.no/role#> .\n" +
@@ -64,9 +63,9 @@ public class WorkModelToIndexMapperTest {
                 "@prefix xml: <http://www.w3.org/XML/1998/namespace> .\n" +
                 "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n" +
                 "\n" +
-                "<http://192.168.50.12:8005/publication/p594502562255> a ns2:Publication ;\n" +
+                "<http://192.168.50.12:8005/publication/p594502562255> rdf:type ns2:Publication ;\n" +
                 "    ns2:bibliofilPublicationID \"1549895\" ;\n" +
-                "    ns2:contributor [ a ns2:Contribution ;\n" +
+                "    ns2:contributor [ rdf:type ns2:Contribution ;\n" +
                 "            ns2:agent <http://192.168.50.12:8005/person/h10834700> ;\n" +
                 "            ns2:role ns5:author ] ;\n" +
                 "    ns2:format <http://data.deichman.no/format#Book> ;\n" +
@@ -80,11 +79,14 @@ public class WorkModelToIndexMapperTest {
                 "    ns4:locationSignature \"Rag\" ;\n" +
                 "    ns4:statementOfResponsibility \"Anne B. Ragde ; traduzione di Cristina Falcinella\" .\n" +
                 "\n" +
-                "<http://192.168.50.12:8005/publication/p735933031021> a ns2:Publication ;\n" +
+                "<http://192.168.50.12:8005/publication/p735933031021> rdf:type ns2:Publication ;\n" +
                 "    ns2:bibliofilPublicationID \"0626460\" ;\n" +
-                "    ns2:contributor [ a ns2:Contribution ;\n" +
+                "    ns2:contributor [ rdf:type ns2:Contribution ;\n" +
                 "            ns2:agent <http://192.168.50.12:8005/person/h10834700> ;\n" +
                 "            ns2:role ns5:author ] ;\n" +
+                "    ns2:contributor [ rdf:type ns2:Contribution ;\n" +
+                "            ns2:agent <http://192.168.50.12:8005/person/h11234> ;\n" +
+                "            ns2:role ns5:translator ] ;\n" +
                 "    ns2:format <http://data.deichman.no/format#Book> ;\n" +
                 "    ns2:isbn \"82-495-0272-8\" ;\n" +
                 "    ns2:language <http://lexvo.org/id/iso639-3/nob> ;\n" +
@@ -92,11 +94,15 @@ public class WorkModelToIndexMapperTest {
                 "    ns2:publicationOf <http://192.168.50.12:8005/work/w4e5db3a95caa282e5968f68866774e20> ;\n" +
                 "    ns2:publicationYear \"2004\"^^xsd:gYear ;\n" +
                 "    ns2:recordID \"11\" ;\n" +
-                "    ns2:subtitle \"roman\" .\n" +
+                "    ns2:subtitle \"roman\" ;\n" +
+                "    ns4:locationSignature \"Rag\" ;\n" +
+                "    ns4:publicationHistory \"Forts. i: Eremittkrepsene\" ;\n" +
+                "    ns4:statementOfResponsibility \"Anne Birkefeldt Ragde\" .\n" +
                 "\n" +
-                "<http://192.168.50.12:8005/work/w4e5db3a95caa282e5968f68866774e20> a ns2:Work ;\n" +
+                "\n" +
+                "<http://192.168.50.12:8005/work/w4e5db3a95caa282e5968f68866774e20> rdf:type ns2:Work ;\n" +
                 "    ns2:audience <http://data.deichman.no/audience#adult> ;\n" +
-                "    ns2:contributor [ a ns2:Contribution,\n" +
+                "    ns2:contributor [ rdf:type ns2:Contribution,\n" +
                 "                ns2:MainEntry ;\n" +
                 "            ns2:agent <http://192.168.50.12:8005/person/h10834700> ;\n" +
                 "            ns2:role ns5:author ] ;\n" +
@@ -105,13 +111,17 @@ public class WorkModelToIndexMapperTest {
                 "        <http://data.deichman.no/literaryForm#novel> ;\n" +
                 "    ns2:mainTitle \"Berlinerpoplene\" .\n" +
                 "\n" +
-                "<http://192.168.50.12:8005/person/h10834700> a ns2:Person ;\n" +
+                "<http://192.168.50.12:8005/person/h10834700> rdf:type ns2:Person ;\n" +
                 "    ns2:birthYear \"1957\" ;\n" +
                 "    ns2:name \"Ragde, Anne B.\" ;\n" +
                 "    ns2:nationality <http://data.deichman.no/nationality#n> ;\n" +
                 "    ns2:personTitle \"forfatter\" ;\n" +
                 "    ns4:lifeSpan \"1957-\" ;\n" +
-                "    ns1:bibliofilPersonId \"10834700\" .\n";
+                "    ns1:bibliofilPersonId \"10834700\" .\n" +
+                "\n" +
+                "<http://192.168.50.12:8005/person/h11234> rdf:type ns2:Person ;\n" +
+                "    ns2:name \"Falcinella, Cristina\" ;\n" +
+                "    ns2:nationality <http://data.deichman.no/nationality#ita> .\n";
 
         Model model = RDFModelUtil.modelFrom(inputGraph, Lang.TURTLE);
 
